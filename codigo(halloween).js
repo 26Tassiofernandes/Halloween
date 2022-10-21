79b5f99a9c7f13ab526eb7b028e89ecc8bd00273
Tássio <86972667+26Tassiofernandes@users.noreply.github.com>
2022-10-21 19:06:10 -0300

    Create codigo(halloween).js

---
+++ codigo(halloween).js
@@ -0,0 +1,34 @@
+function um() {
+    img = document.getElementById('imagem')
+    msg = document.getElementById('msg')
+    img.src = 'neymar.png'
+    msg.style.color = 'white'
+    msg.innerHTML = 'Ney balada'
+
+}
+
+function dois() {
+    img = document.getElementById('imagem')
+    msg = document.getElementById('msg')
+    img.src = 'ney abelha.png'
+    msg.style.color = 'white'
+    msg.innerHTML = '???'
+    
+}
+
+function tres() {
+    img = document.getElementById('imagem')
+    msg = document.getElementById('msg')
+    img.src = 'ney elvis KKK.png'
+    msg.style.color = 'white'
+    msg.innerHTML = 'Ney elvis rsrs'
+}
+
+function quatro() {
+    img = document.getElementById('imagem')
+    msg = document.getElementById('msg')
+    img.src = 'ney vaca .png'
+    msg.style.color = 'white'
+    msg.innerHTML = 'Ney vaca tá de sacanagem!'
+
+}
